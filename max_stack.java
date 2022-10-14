65c43c46267f12d885d1e50ea9471f4dcde20d3e
Shasika Udayanga <shasikaudayanga@gmail.com>
2022-10-14 19:59:52 +0530

    Create max_stack.java

---
+++ max_stack.java
@@ -0,0 +1,51 @@
+public class MaxStack {
+
+    public MaxStack() {};
+    private class Node{
+        private int value;
+        private Node nextNode;
+        private Node prevMaxNode;
+    }
+    private Node maxNode;
+    private Node stackHead;
+    public void push(int x) throws Exception {
+        Node newNode = new Node();
+        newNode.value = x;
+        if (stackHead == null){
+            stackHead = newNode;
+        } else {
+            newNode.nextNode = stackHead;
+            stackHead = newNode;
+        }
+        if (maxNode == null || newNode.value > maxNode.value){
+            newNode.prevMaxNode = maxNode;
+            maxNode = newNode;
+        }
+        this.printStack();
+    }
+
+    public int pop(){
+        int popValue = stackHead.value;;
+        if (stackHead == null){
+            return -1;
+        }
+        if (stackHead.prevMaxNode != null){
+            maxNode = stackHead.prevMaxNode;
+        }
+        stackHead = stackHead.nextNode;
+        return popValue;
+    }
+
+    public int max(){
+        return maxNode.value;
+    }
+
+    private void printStack(){
+        System.out.println("----- STACK ----");
+        Node temp = stackHead;
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.nextNode;
+        }
+    }
+}
