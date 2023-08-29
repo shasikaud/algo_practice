48b253a4747c567b4e69762ee70b7038c1f99915
Shasika Udayanga <shasikaudayanga@gmail.com>
2023-08-29 19:02:45 +0530

    Create search_in_a_binary_tree.java

---
+++ search_in_a_binary_tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root==null || root.val == val) return root;
+
+        Stack<TreeNode> nodes = new Stack<TreeNode>();
+        nodes.push(root);
+
+        while (!nodes.empty()) {
+            TreeNode currNode = nodes.pop();
+            if (currNode.val == val) return currNode;
+            if (currNode.left != null) nodes.push(currNode.left);
+            if (currNode.right != null) nodes.push(currNode.right);
+        }
+
+        return null;
+        
+    }
+}
