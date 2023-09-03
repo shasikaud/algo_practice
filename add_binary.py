12b7d6957d1a58ae95ffee8ed48f452c330e6925
Shasika Udayanga <shasikaudayanga@gmail.com>
2023-09-03 12:56:48 +0530

    added example notes

--- add_binary.py
+++ add_binary.py
@@ -1,3 +1,14 @@
+'''
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+'''
+
 class Solution(object):
     def addBinary(self, a, b):
         """
